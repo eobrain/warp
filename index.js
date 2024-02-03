4d03077dc68ae47bc6200f3e23483fdd817c561c
Eamonn O'Brien-Strain <eobrain@gmail.com>
2024-02-02 20:33:15 -0800

    Add comments

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { Perspective } from './view.js'
 import { Controls } from './controls.js'
 import { timeString } from './time.js'
 
+// Values entered by the user through the UI
 const controls = new Controls()
 
 const ctx = $canvas.getContext('2d')
@@ -11,8 +12,8 @@ const ctx = $canvas.getContext('2d')
 // All units are SI (meters, kilograms, etc.) unless suffix added
 
 //  const SUN_MASS = 1.9891e30
-const JUPITER_MASS = 1.898e27
-const AU = 149597870700 // distance from Earth to Sun
+const JUPITER_MASS = 1.898e27 // in Kg
+const AU = 149597870700 // distance from Earth to Sun in metres
 const MINUTE = 60
 const HOUR = MINUTE * 60
 const DAY = HOUR * 24
@@ -22,16 +23,23 @@ const DAY = HOUR * 24
 const FRAMES_PER_SECOND = 250
 const SECONDS_PER_FRAME = 1 / FRAMES_PER_SECOND
 
-// Dimensions
+// Dimension indices
 const X = 0
 const Y = 1
 const Z = 2
 const D = [X, Y, Z]
 const D2 = [X, Y]
 
+// Mass of each object initially before collisions (in Kg)
 const initialMass = controls.jupiters * JUPITER_MASS
+
+// Dimensions of the view in pixels
 const VIEWPORT_SIZE = [...D].map(_ => 500)
+
+// Dimension of the space (metres)
 const SIZE = [...D].map(_ => AU)
+
+// Total mass in Kg of all the objects
 const TOTAL_MASS = initialMass * controls.n
 
 const ORBIT_TIME = 7 * DAY
@@ -50,45 +58,67 @@ const view = new Perspective(SIZE[0] / VIEWPORT_SIZE[0], SIZE)
 
 const FRAME_COLOR = 'green'
 
+// A massive spherical object moving in space
 class Particle {
   constructor () {
+    // Position vector
     this.p = [...D].map((_, i) => SIZE[i] / 4 + Math.random() * SIZE[i] / 2)
     const pFromCenter = this.p.map((p, i) => p - SIZE[i] / 2)
     const dFromCenter = Math.sqrt(pFromCenter.reduce((acc, val) => acc + val * val, 0))
+
+    // Velocity vector
     this.v = [...D2].map((_, i) => ANGULAR_VELOCITY * dFromCenter * pFromCenter[1 - i] / (SIZE[i] ** 2))
     this.v[X] *= -1
     this.v[Z] = 0
+
+    // Mass
     this.m = initialMass
+
+    // Acceleration vector
     this.acceleration = [...D].map(_ => 0)
+
+    // radius
     this.radius = radius(this.m)
+
+    // Position vector after next time delta
     this.nextP = [...D]
+
+    // Velocity vector after next time delta
     this.nextV = [...D]
   }
 
+  // Draw this object in and HTML canvas
   draw () {
+    // Set color according to velocity in the Z direction (doppler effect)
     let hue = 0
     let saturation = 0
     if (this.v[Z] > 0) {
+      // Moving away so red shift
       hue = 0 // red
       saturation = Math.trunc(100 * this.v[Z] / maxVz)
     } else if (this.v[Z] < 0) {
+      // moving towards so blue shift
       hue = 240 // blue
       saturation = Math.trunc(100 * this.v[Z] / minVz)
     }
     const lightness = 100 - Math.trunc(this.p[Z] * 50 / SIZE[Z])
-
-    const rPix = view.transformSize(this.radius, this.p[Z])
-    const [xPix, yPix] = view.transform(...this.p)
-
     ctx.fillStyle = `hsl(${hue} ${saturation}% ${lightness}%)`
 
+    // Apply perspective transforms to the radius and the position to get the pixel coordinates
+    const rPix = view.transformSize(this.radius, this.p[Z])
+    const [xPix, yPix] = view.transform(...this.p)
     const [, yBasePix] = view.transform(this.p[X], SIZE[Y], this.p[Z])
+
+    // Set the color
     ctx.strokeStyle = `rgba(100%, 100%, 100%, ${100 * Math.sqrt(this.m / TOTAL_MASS)}%)`
+
+    // Deaw the vertical line from the bottom of the space to the paricle
     ctx.beginPath()
     ctx.moveTo(xPix, yPix)
     ctx.lineTo(xPix, yBasePix)
     ctx.stroke()
 
+    // Draw the particle as a circle
     ctx.beginPath()
     ctx.arc(
       xPix,
@@ -98,20 +128,26 @@ class Particle {
     ctx.fill()
   }
 
+  // Add acceleration contribution caused other object
   updateAcceleration (other) {
+    // Distance vector between this object and other object, taking wrapping into account
     const dp = [...D].map(i => {
-      if (!controls.wrap) {
-        return this.p[i] - other.p[i]
-      }
+      // Normally the distance is just the vector difference
       const d = this.p[i] - other.p[i]
-      if (d > SIZE[i] / 2) return d - SIZE[i]
-      if (d < -SIZE[i] / 2) return d + SIZE[i]
+      if (controls.wrap) {
+        // When wrapping the closest distance may be wrapping around
+        if (d > SIZE[i] / 2) return d - SIZE[i]
+        if (d < -SIZE[i] / 2) return d + SIZE[i]
+      }
       return d
     })
 
+    // Euclidean dstance between this object and other
     const r = Math.sqrt(dp.reduce(
       (acc, curr) => acc + curr * curr,
       0))
+
+    // Handle collisions by merging two objects, marking one as to be deleted
     if (r < this.radius + other.radius) {
       // collided
       this.deleted = true
@@ -123,11 +159,14 @@ class Particle {
       other.radius = radius(other.m)
       return
     }
+
+    // Calculate incremental acceleration according to Newton's law of gravitation
     for (const i in D) {
       this.acceleration[i] += -dp[i] * other.m * G / (r * r * r)
     }
   }
 
+  // Update acceleration, velocity, and position
   update (dt) {
     for (const i in D) {
       this.acceleration[i] = 0
@@ -152,8 +191,12 @@ class Particle {
   }
 
   tick (dt) {
+    // Update, without considering edges
     this.update(dt)
+
+    // Handle edges by either wrapping or bouncing
     if (controls.wrap) {
+      // Wrap by adding or subtracting the height, width, or depth of the space when the object goes beyond the edge
       for (const i in D) {
         while (this.nextP[i] < 0) {
           this.nextP[i] += SIZE[i]
@@ -163,6 +206,7 @@ class Particle {
         }
       }
     } else {
+      // Bounce by inverting the velocity vector component in the dimension where the object went past the edge
       for (const i in D) {
         if (this.nextP[i] < 0 || this.nextP[i] > SIZE[i]) {
           // Bounce off wall
@@ -173,8 +217,10 @@ class Particle {
   }
 }
 
+// Initialize all the particles
 let particles = [...Array(controls.n)].map(_ => new Particle())
 
+// The cubic outline of the space
 const FRAME_PATH = [
   [0, 0, 0],
   [0, 0, 1],
@@ -190,11 +236,14 @@ const FRAME_PATH = [
   [0, 0, 1]
 ].map(pu => pu.map((u, i) => u * SIZE[i]))
 
+// Draw the cubic outline of the space
 function drawFrame () {
   ctx.strokeStyle = FRAME_COLOR
   ctx.beginPath()
   for (let i = 0; i < FRAME_PATH.length; ++i) {
+    // Apply the perspective transform to the path to pixel coordinatesz`
     const [xPix, yPix] = view.transform(...FRAME_PATH[i])
+
     if (i === 0) {
       ctx.moveTo(xPix, yPix)
     } else {
@@ -204,22 +253,33 @@ function drawFrame () {
   ctx.stroke()
 }
 
+// Initialize
 let time = 0
-
 let frame = 0
 let lastMs = Date.now()
+
+// Function called on every frame
 function draw () {
   const dt = SECONDS_PER_FRAME * controls.speedup
 
+  // Set black background
   ctx.fillStyle = 'black'
   ctx.fillRect(0, 0, SIZE[X], SIZE[Y])
+
+  // Draw the outline of the space
   drawFrame()
+
+  // Deaw the particles
   for (const particle of particles) {
     particle.draw()
   }
+
+  // calculate new velocies and positions
   for (const particle of particles) {
     particle.tick(dt)
   }
+
+  // Update postions and velocites with new values
   for (const i in D) {
     for (const particle of particles) {
       particle.p[i] = particle.nextP[i]
@@ -227,8 +287,13 @@ function draw () {
     }
   }
 
+  // Remove particles deleted by collisions
   particles = particles.filter(particle => !particle.deleted)
+
+  // Sort in Z order so nearer objects are drawn on top of farthur objeces
   particles.sort((a, b) => b.p[Z] - a.p[Z])
+
+  // Update framecount and time
   ++frame
   if (frame % FRAMES_PER_SECOND === 0) {
     const currentMs = Date.now()
@@ -237,6 +302,8 @@ function draw () {
     lastMs = currentMs
   }
   time += dt
+
+  // Update HTML to show time and object count
   $time.innerText = timeString(time)
   $count.innerText = `${particles.length} objects`
 }
